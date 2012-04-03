ecd4ed2fd03b8adbe44420b234fe58b3757a9ca3
Sergiu Niculescu <contact.sergiuniculescu@gmail.com>
2012-04-03 18:01:24 +0300

    deleted:    scripts/local_scripts/sync_pacman.sh

--- scripts/local_scripts/sync_pacman.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fakedb=/dev/shm/fakepacdb
-realdb=/var/lib/pacman
-
-[[ ! -d $fakedb ]] && { mkdir -p "$fakedb/sync" || exit 1; }
-[[ ! -L $fakedb/local ]] && { ln -s "$realdb/local" "$fakedb" || exit 2; }
-
-exec fakeroot pacman --dbpath "$fakedb" -Sy
