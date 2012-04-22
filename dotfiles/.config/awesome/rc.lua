3dd9527480262ea954cd1c1b4215ce3057d3b012
Sergiu Niculescu <sergiu@sergiu-desktop.localdomain>
2012-04-23 00:41:14 +0300

    modified:   dotfiles/.Xresources 	modified:   dotfiles/.config/awesome/rc.lua 	modified:   dotfiles/.config/ranger/__pycache__/apps.cpython-32.pyo 	modified:   dotfiles/.config/ranger/__pycache__/commands.cpython-32.pyo 	modified:   dotfiles/.config/ranger/__pycache__/options.cpython-32.pyo 	modified:   dotfiles/.config/ranger/bookmarks 	modified:   dotfiles/.config/ranger/history 	modified:   dotfiles/.vimrc 	renamed:    dotfiles/.vimrc -> dotfiles/.vimrc2

--- dotfiles/.config/awesome/rc.lua
+++ dotfiles/.config/awesome/rc.lua
@@ -46,17 +46,17 @@ altkey = "Mod1"
 -- {{{ Table of layouts to cover with awful.layout.inc, order matters.
 layouts =
 {
-    awful.layout.suit.tile,							--1
-    awful.layout.suit.tile.bottom,			--2
-    awful.layout.suit.fair,							--3
-    awful.layout.suit.tile.top,					--4
-    awful.layout.suit.spiral,						--5
-    awful.layout.suit.magnifier,				--6
-    awful.layout.suit.max,							--7
-    awful.layout.suit.floating,					--8
-		awful.layout.suit.tile.left,				--9
-		awful.layout.suit.fair.horizontal, 	--10
-		awful.layout.suit.spiral.dwindle  	--12	
+    awful.layout.suit.tile,             --1
+    awful.layout.suit.tile.bottom,      --2
+    awful.layout.suit.fair,             --3
+    awful.layout.suit.tile.top,         --4
+    awful.layout.suit.spiral,           --5
+    awful.layout.suit.magnifier,        --6
+    awful.layout.suit.max,              --7
+    awful.layout.suit.floating,         --8
+    awful.layout.suit.tile.left,        --9
+    awful.layout.suit.fair.horizontal,  --10
+    awful.layout.suit.spiral.dwindle    --12
 }
 -- }}}
 
@@ -64,7 +64,7 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     tags[s] = awful.tag({ "term", "web", "chat", "irc", "misc" }, s, 
-		{layouts[1], layouts[7], layouts[7], layouts[7], layouts[2]})
+            {layouts[1], layouts[7], layouts[7], layouts[7], layouts[2]})
 end
 -- }}}
 
@@ -74,16 +74,16 @@ root.buttons(awful.util.table.join(
     awful.button({ }, 5, awful.tag.viewnext),
     awful.button({ }, 4, awful.tag.viewprev)))
 -- }}}
-		
+
 -- {{{ GLOBAL KEYS BINDINGS
 globalkeys = awful.util.table.join(
-	-- Focus
+    -- Focus
     awful.key({ modkey }, "Left",
         function () awful.client.focus.byidx(-1) if client.focus then client.focus:raise() end end),
     awful.key({ modkey }, "Right",
         function () awful.client.focus.byidx( 1) if client.focus then client.focus:raise() end end),
 
-	-- Tag control
+    -- Tag control
 ---   awful.key({ altkey, "Control" }, "Left",  awful.tag.viewprev ),
 ---   awful.key({ altkey, "Control" }, "Right", awful.tag.viewnext ),
     awful.key({ modkey,           }, "Up",    function () awful.tag.incmwfact( 0.05)    end),
@@ -97,16 +97,16 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
 
-	-- Awesome restart and quit
+    -- Awesome restart and quit
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Control" }, "q", awesome.quit),
 
-	-- Layout manipulation
+    -- Layout manipulation
     awful.key({ modkey, "Shift" }, "Left",  function () awful.client.swap.byidx( -1) end),
     awful.key({ modkey, "Shift" }, "Right", function () awful.client.swap.byidx( 1)  end),
     awful.key({ modkey,         }, "o",     awful.client.urgent.jumpto),
-   
-	-- Program shortcuts
+
+    -- Program shortcuts
     awful.key({ modkey, }, "t", function () exec("tv-maxe", false) end),
     awful.key({ modkey, }, "x", function () exec(terminal) end),
     awful.key({ modkey  }, "z", function () awful.util.spawn("dmenu_run") end),
@@ -117,9 +117,9 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, }, "p", function () exec(instant_messenger, false) end),
     awful.key({ modkey, }, "i", function () exec(irc, false) end),
 
-	--awful.key({ modkey, }, "n", function () exec(music_player) end),
-		awful.key({ modkey, }, "u", function () exec("urxvtc -e bash -c 'sudo pacman-color -Syu'") end),
-		awful.key({ modkey, }, "y", function () exec("urxvtc -e bash -c 'sudo yaourt -Syua'") end),
+    --awful.key({ modkey, }, "n", function () exec(music_player) end),
+    awful.key({ modkey, }, "u", function () exec("urxvtc -e bash -c 'sudo pacman-color -Syu'") end),
+    awful.key({ modkey, }, "y", function () exec("urxvtc -e bash -c 'sudo yaourt -Syua'") end),
     awful.key({ modkey, }, "h", function () exec(system_monitor) end),
     awful.key({ modkey, }, "e", function () exec(gui_editor) end),
     awful.key({ modkey, }, "n", function () exec(music_player, false) end),
@@ -129,20 +129,20 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, altkey }, "a", function () exec(terminal .. " -e alsamixer", false) end),
     awful.key({ }, "XF86PowerOff", function () exec("sudo poweroff", false) end),
     -- awful.key({ }, "XF86PowerOff", function () exec("sudo pm-hibernate", false) end),
-		awful.key({ }, "Print", function () awful.util.spawn("scrot -e 'mv $f ~/Pictures/Screenshots/ 2>/dev/null'") end),
-		
-	-- Volume Control
+    awful.key({ }, "Print", function () awful.util.spawn("scrot -e 'mv $f ~/Pictures/Screenshots/ 2>/dev/null'") end),
+
+    -- Volume Control
     awful.key({ }, "XF86AudioRaiseVolume", function () volumecfg.up() end),
     awful.key({ }, "XF86AudioLowerVolume", function () volumecfg.down() end),
     awful.key({ }, "XF86AudioMute",        function () volumecfg.toggle() end),
 
-	-- MPD control
+    -- MPD control
     awful.key({	modkey, "Control" }, "Up", function () exec("mpc play", false) end),
     awful.key({	modkey, "Control" }, "Down", function () exec("mpc pause", false) end),
     awful.key({ modkey, "Control" }, "Right" , function () exec("mpc next", false) end),
     awful.key({	modkey, "Control" }, "Left" , function () exec("mpc prev", false) end),
 
-	-- Menu
+    -- Menu
     awful.key({ modkey }, "Escape", function () mymainmenu:toggle(keymenu_args) end),
     awful.key({ altkey }, "Tab",    function () awful.menu.menu_keys.down = { "Down", "Alt_L" }
                                                 local cmenu = awful.menu.clients({width=245}, keymenu_args ) end)
@@ -150,48 +150,48 @@ globalkeys = awful.util.table.join(
 
 --/// CLIENT KEYS
 clientkeys = awful.util.table.join(
- 	 -- Kill
+     -- Kill
     awful.key({ modkey }, "c", function (c) c:kill() end),
-	 -- Floating
+     -- Floating
     awful.key({ altkey }, "space", awful.client.floating.toggle ),
-	 -- Swap to master
+     -- Swap to master
     awful.key({ altkey }, "Return", function (c) c:swap(awful.client.getmaster()) end),
-	 -- Redraw
+     -- Redraw
     awful.key({ modkey }, "F1", function (c) c:redraw() end),
-	 -- Fullscreen
+     -- Fullscreen
     awful.key({ modkey }, "F11", function (c) c.fullscreen = not c.fullscreen  end),
-	 -- On Top
+     -- On Top
     awful.key({ modkey }, "F5", function (c) c.ontop = not c.ontop end),
-	 -- Remove Titlebars
+     -- Remove Titlebars
     awful.key({ modkey }, "F3", function (c) if c.titlebar then awful.titlebar.remove(c) else awful.titlebar.add(c) end end),
-	 -- Move to center
+     -- Move to center
     awful.key({ modkey }, "#84", function (c) awful.placement.centered(c) end),
     awful.key({ modkey }, "#79", function (c) awful.placement.center_horizontal(c) end),
     awful.key({ modkey }, "#87", function (c) awful.placement.center_vertical(c) end),
-	 
-	 -- Toggle full maximize
+
+     -- Toggle full maximize
     awful.key({ modkey }, "a", function (c) c.maximized_horizontal, c.maximized_vertical = not c.maximized_horizontal, not c.maximized_vertical end),
-	 -- Toggle vertical maximize
+     -- Toggle vertical maximize
     awful.key({ modkey }, "KP_Next", function (c) c.maximized_vertical = not c.maximized_vertical end),
-	-- Toggle horizontal maximize
+    -- Toggle horizontal maximize
     awful.key({ modkey }, "KP_Prior", function (c) c.maximized_horizontal = not c.maximized_horizontal end),  
 
-	-- Move clients (up,down,left,right)
+    -- Move clients (up,down,left,right)
     awful.key({ modkey }, "#80", function (c) awful.client.moveresize(  0,-30, 0, 0) end),
     awful.key({ modkey }, "#88", function (c) awful.client.moveresize(  0, 30, 0, 0) end), 
     awful.key({ modkey }, "#83", function (c) awful.client.moveresize(-30, 0,  0, 0) end), 
     awful.key({ modkey }, "#85", function (c) awful.client.moveresize( 30, 0,  0, 0) end),
-	-- To margins (up,down,left,right)
-	awful.key({modkey,altkey}, "#80", function (c) local g = c:geometry(); g.y = 16; c:geometry(g) end), 
-	awful.key({modkey,altkey}, "#88", function (c) local g, screen_area = c:geometry(), screen[c.screen].workarea
-											g.y = screen_area.y + screen_area.height - g.height 
-											c:geometry(g) end), 
-	awful.key({modkey,altkey}, "#83", function (c) local g = c:geometry(); g.x= 0; c:geometry(g) end), 
-	awful.key({modkey,altkey}, "#85", function (c) local g, screen_area = c:geometry(), screen[c.screen].workarea
-											g.x = screen_area.x + screen_area.width - g.width 
-											c:geometry(g) end), 
+    -- To margins (up,down,left,right)
+    awful.key({modkey,altkey}, "#80", function (c) local g = c:geometry(); g.y = 16; c:geometry(g) end), 
+    awful.key({modkey,altkey}, "#88", function (c) local g, screen_area = c:geometry(), screen[c.screen].workarea
+                                                        g.y = screen_area.y + screen_area.height - g.height 
+                                                        c:geometry(g) end), 
+    awful.key({modkey,altkey}, "#83", function (c) local g = c:geometry(); g.x= 0; c:geometry(g) end), 
+    awful.key({modkey,altkey}, "#85", function (c) local g, screen_area = c:geometry(), screen[c.screen].workarea
+                                                        g.x = screen_area.x + screen_area.width - g.width 
+                                                        c:geometry(g) end), 
 
-	-- Resize clients (up,down,left,right)
+    -- Resize clients (up,down,left,right)
     awful.key({ altkey, }, "#80", function (c) awful.client.moveresize( 0, 0,  0,-30) end),
     awful.key({ altkey, }, "#88", function (c) awful.client.moveresize( 0, 0,  0, 30) end), 
     awful.key({ altkey, }, "#83", function (c) awful.client.moveresize( 0, 0,-30, 0 ) end), 
@@ -257,23 +257,23 @@ awful.rules.rules = {
                      focus = true,
                      keys = clientkeys,
                      buttons = clientbuttons,
-	     	         size_hints_honor = true }, },
---	{ rule = { class = 'URxvt' }, 
---			properties = { size_hints_honor = false, tag = tags [1][1] }, },
-	{ rule = { class = "Firefox" },
-	    properties = { tag = tags[1][2] }, },
-	{ rule = { class = "Pidgin" },
-	    properties = { tag = tags[1][3] }, },
-	{ rule_any = { class = { "Pcmanfm", "Tv-maxe" }, },
-	    properties = { tag = tags[1][5] }, },
-	{ rule = { name = "Finch" }, 
-		  properties = { tag = tags[1][3] }, },
-	{ rule = { name = "Weechat" }, 
-		  properties = { tag = tags[1][4] }, },
-	{ rule = { name = "SmallTerm" }, 
-		  properties = { floating = true, ontop = true }, },
-	{ rule_any = { class = { "MPlayer", "Gnome-mplayer", "sxiv", "feh", "Viewnior" }, },         
-	    properties = { floating = true }, },
+                        size_hints_honor = true }, },
+   -- { rule = { class = 'URxvt' },
+     --   properties = { size_hints_honor = false, tag = tags [1][1] }, },
+    { rule = { class = "Firefox" },
+        properties = { tag = tags[1][2] }, },
+    { rule = { class = "Pidgin" },
+        properties = { tag = tags[1][3] }, },
+    { rule_any = { class = { "Pcmanfm", "Tv-maxe", "medit" }, },
+        properties = { tag = tags[1][5] }, },
+    { rule = { name = "Finch" },
+        properties = { tag = tags[1][3] }, },
+    { rule = { name = "Weechat" },
+        properties = { tag = tags[1][4] }, },
+    { rule = { name = "SmallTerm" },
+        properties = { floating = true, ontop = true }, },
+    { rule_any = { class = { "MPlayer", "Gnome-mplayer", "sxiv", "feh", "Viewnior" }, },
+        properties = { floating = true }, },
 
 }
 -- }}}
@@ -340,7 +340,7 @@ local function run_once(process, cmd)
 
    for p in processwalker() do
       if p:find(process:gsub("[-+?*]", regex_killer)) then
-	 return
+         return
       end
    end
    return awful.util.spawn(cmd or process)
