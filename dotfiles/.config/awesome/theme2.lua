db26ab3c08daaf4c9e4f4f500e9b8ccc83186796
Sergiu Niculescu <sergiu@sergiu-desktop.localdomain>
2012-06-04 03:03:46 +0300

    modified:   dotfiles/.config/awesome/rc.lua 	modified:   dotfiles/.config/awesome/theme2.lua 	new file:   dotfiles/.config/awesome/wallpapers/Linux.jpg

--- dotfiles/.config/awesome/theme2.lua
+++ dotfiles/.config/awesome/theme2.lua
@@ -1,6 +1,37 @@
 --///  Main
 theme = {"arch"}
-theme.wallpaper_cmd = { "awsetbg -l /home/sergiu/.config/awesome/wallpapers/black-cat-wallpaper-2.jpg" }
+--theme.wallpaper_cmd = { "awsetbg -a /home/sergiu/.config/awesome/wallpapers/responsibleparents.jpg" }
+theme.wallpaper_cmd = { "awsetbg -r /home/sergiu/Pictures/National_Geographic/" }
+--//
+
+--/// Change the wallpaper randomly after a (relatively random) time
+-- seed and "pop a few"
+math.randomseed( os.time())
+for i=1,1000 do tmp=math.random(0,1000) end
+
+x = 0
+
+-- setup the timer
+ mytimer = timer { timeout = x }
+ mytimer:add_signal("timeout", function()
+
+-- tell awsetbg to randomly choose a wallpaper from your wallpaper directory
+ os.execute("awsetbg -r /home/sergiu/Pictures/National_Geographic/")
+
+-- stop the timer (we don't need multiple instances running at the same time)
+ mytimer:stop()
+
+-- define the interval in which the next wallpaper change should occur in seconds
+-- (in this case anytime between 10 and 20 minutes)
+ x = math.random(600, 1200)
+
+--restart the timer
+ mytimer.timeout = x
+ mytimer:start()
+end)
+
+-- initial start when rc.lua is first run
+ mytimer:start()
 --//
 
 --/// Styles
