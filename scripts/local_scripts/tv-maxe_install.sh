adc8161644ddb4b915dc995ab7a50aa45ea936ca
Sergiu Niculescu <contact.sergiuniculescu@gmail.com>
2013-03-12 20:43:12 +0200

    Updated but still not functional

--- scripts/local_scripts/tv-maxe_install.sh
+++ scripts/local_scripts/tv-maxe_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+user=sergiu
 sudo zypper install python-imaging
 wget http://sopcast-player.googlecode.com/files/sp-auth-3.2.6.tar.gz
 tar -zxvf sp-auth-3.2.6.tar.gz
@@ -16,12 +17,11 @@ sudo cp libstdc++.so.5.0.1 /usr/lib/
 rm -rf libstdcpp5
 cd /usr/lib/
 sudo ln -s libstdc++.so.5.0.1 libstdc++.so.5
-cd ~
+cd /home/$user/
 wget http://tv-maxe.googlecode.com/files/tv-maxe-0.08.tar.gz
 tar -zxvf tv-maxe-0.08.tar.gz
+mv tv-maxe-0.08 /home/$user/.tv-maxe
 rm tv-maxe-0.08.tar.gz
-mv tv-maxe-0.08 ~/.tv-maxe
-cd ~/.tv-maxe
-python tvmaxe.py
-echo -e '#!/bin/bash\ncd /home/$USER/.tv-maxe && python tvmaxe.py &' | tee ~/.tv-maxe/tvmaxe.sh
-echo -e '[Desktop Entry]\nName=TV-Maxe\nGenericName=TV-Maxe\nComment=tv-internet\nExec=/home/$USER/.tv-maxe/tvmaxe.sh\nIcon=/home/$USER/.tv-maxe/tvmaxe.png\nTerminal=false\nType=Application\nCategories=AudioVideo;Player;' | sudo tee /usr/share/applications/tv-maxe.desktop
+echo -e '#!/bin/bash\ncd /home/sergiu/.tv-maxe && python tvmaxe.py &' | tee /home/$user/.tv-maxe/tvmaxe.sh
+chown -R $user /home/$user/.tv-maxe
+echo -e '[Desktop Entry]\nName=TV-Maxe\nGenericName=TV-Maxe\nComment=tv-internet\nExec=/home/sergiu/.tv-maxe/tvmaxe.sh\nIcon=/home/sergiu/.tv-maxe/tvmaxe.png\nTerminal=false\nType=Application\nCategories=AudioVideo;Player;' | sudo tee /usr/share/applications/tv-maxe.desktop
